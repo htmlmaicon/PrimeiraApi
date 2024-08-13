e7695cd9b3761329d920d836cda4a93526044c7c
MAICON TAVARES <engs-maiconpereira@camporeal.edu.br>
2024-08-13 20:46:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css"
 
 function App() {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    let url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setApiData([json])) // Wrapping json in an array
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <strong>NASA</strong>
       </header>
+
+      {apiData.map((item) => (
+        <article key={item.title}>
+          <strong>{item.title}</strong>
+          <img src={item.url} alt={item.title} />
+          <p>{item.explanation}</p>
+          <button>Acessar</button>
+        </article>
+      ))}
     </div>
   );
 }
